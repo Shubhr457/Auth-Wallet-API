6c981cf9f8aa1df4aec519d934df06fa14aaafa9
Shubhr457 <136572711+Shubhr457@users.noreply.github.com>
2024-07-12 00:20:09 +0530

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,10 +1,6 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 mongoose.set("strictQuery", true);
 
-async function connectToMongoDB(url) {
+export async function connectToMongoDB(url) {
   return mongoose.connect(url);
 }
-
-module.exports = {
-  connectToMongoDB
-};
